b1d6dc1fef8790bb29efd25f002247956d59497b
micheleboanno92 <michelebonanno92@gmail.com>
2024-11-18 16:47:41 +0100

    Utilizzato Model Project nella risposta in formato json

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -5,19 +5,23 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// Models
+use App\Models\Project;
+
 class ProjectController extends Controller
 {
     public function index() 
     {
+        $projects = Project::get();
+
         return response()->json([
         'success' => true,
         'code' => 200,
         'message' => 'OK',
         'data' => [
-            1,
-            2,
-            3
+            'projects' => $projects
         ],
+        // richiamato e utilizzato il Model Project per avere nella risposta per avere tutti i project esistente nel db come risposta in formato json
 
         ]);
     }
